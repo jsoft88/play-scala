8447ce186d8a28f8708dc2f498e4647f68b2bdaf
jsoft88 <jorgitouni@gmail.com>
2017-02-28 07:56:34 -0300

    Added tweet and batch models. Added method to insert new record into tweet's table. Removed previous models.

--- app/models/Tweet.scala
+++
@@ -1,7 +0,0 @@
-package models
-
-case class Tweet(tweetId: String, userAlias: String, date: Long, sentiment: double, batchId: Int)
-
-case class Page[A](items: Seq[A], page: Int, offset: Long, total: Long) {
-
-}
